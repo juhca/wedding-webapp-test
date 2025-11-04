745dfe75a76e24bb8038a16ab76a71d7be5aa9cb
Tomas Štefe <tomas.stefe@gmail.com>
2025-11-04 18:33:47 +0100

    [BACKEND] - Adding user roles

--- backend/WeddingApp-Test.Domain/Entities/User.cs
+++ backend/WeddingApp-Test.Domain/Entities/User.cs
@@ -3,13 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WeddingApp_Test.Domain.Enums;
 
 namespace WeddingApp_Test.Domain.Entities;
 
 public class User
 {
 	public Guid Id { get; set; }
+	
 	public string Name { get; set; } = string.Empty;
 	public string LastName { get; set; } = string.Empty;
 	public string Email { get; set; } = string.Empty;
+	
+	// only admins have password-based login; guest will use access codes
+	public string? PasswordHash { get; set; } = string.Empty;
+	public string? AccessCode { get; set; } = string.Empty;
+	
+	public UserRole Role { get; set; }
+	
 }
--- backend/WeddingApp-Test.Infrastructure/Persistence/AppDbContext.cs
+++ backend/WeddingApp-Test.Infrastructure/Persistence/AppDbContext.cs
@@ -6,4 +6,14 @@ namespace WeddingApp_Test.Infrastructure.Persistence;
 public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
 {
 	public DbSet<User> Users => Set<User>();
+
+	protected override void OnModelCreating(ModelBuilder modelBuilder)
+	{
+		base.OnModelCreating(modelBuilder);
+
+		// enum will be saved as a string to db
+		modelBuilder.Entity<User>()
+			.Property(u => u.Role)
+			.HasConversion<string>();
+	}
 }
